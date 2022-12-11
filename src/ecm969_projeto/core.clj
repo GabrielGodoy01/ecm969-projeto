6d9e8243693e316401c2bbb7428aead5adf94b38
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-12-11 19:04:28 -0300

    Finalizando Projeto

--- src/ecm969_projeto/core.clj
+++ src/ecm969_projeto/core.clj
@@ -1,33 +1,31 @@
 (ns ecm969-projeto.core (:require [io.pedestal.http :as http]
-                                  [environ.core :refer [env]]
-                                  [ecm969-projeto.db :refer [db-posts]]
+                                  [clojure.java.jdbc :as jdbc]
                                   [io.pedestal.http.route :as route]))
 
-(def posts [{:id "0" :text "Este é meu primeiro Post!" :user "0"}
-             {:id "1" :text "Que plataforma incrível" :user ""}
-             {:id "2" :text "Odeio este Gabriel!!!" :user "1"}
-             {:id "3" :text "Clojure reina!!!!!!!!!!!" :user "1"}
-             {:id "4" :text "Obrigado Cidinho!" :user "1"}])
+(def port 5000)
 
-(defn get-post [{{:keys [post]} :path-params
-                 {:keys [extended]} :query-params}]
-  (if-let [post (->> posts
-                     (filter #(= post (:id %)))
-                     first)]
-    {:status 200 :body (if extended post (dissoc post :id))}
-    {:status 404}))
+(def mysql-db {:host "aws-sa-east-1.connect.psdb.cloud"
+               :dbtype "mysql"
+               :dbname "posts"
+               :user "4w0i1y7ur91qkkw6i2bg"
+               :password "pscale_pw_62z6IiZEoAb9Eo2HV3OnBYHXS494jJw8W5Dfa2OUJoe"})
 
-(defn get-posts [_] {:status 200 :body db-posts})
+(def db-posts (jdbc/query mysql-db ["select * from posts"]))
+(def db-delete (jdbc/delete! mysql-db :posts ["id = ?" 1]))
+(def db-create-post (jdbc/insert! mysql-db :posts {:id "2" :text "Próximo Post" :user "2"}))
 
-(def routes #{["/posts/:post" :get get-post :route-name :get-post]
-              ["/posts" :get get-posts :route-name :get-posts]})
+(defn get-posts [_] {:status 200 :body (println db-posts)})
+(defn get-delete [_] (println db-delete) {:status 200 :body "A tabela foi limpa."})
+(defn new-post [_]  (println db-create-post) {:status 200 :body "Um novo post foi criado no banco de dados!"})
+
+(def routes (route/expand-routes #{["/delete" :get get-delete :route-name :delete]
+                                   ["/insert" :get new-post :route-name :insert]
+              ["/posts" :get get-posts :route-name :posts]}))
 
 (def service-map (-> {::http/routes routes
-                      ::http/type   :immutant
+                      ::http/type   :jetty
                       ::http/host   "0.0.0.0"
-                      ::http/join?  false
-                      ::http/port   (Integer. (or (env :port) 5000))}
-                     http/default-interceptors
-                     (update ::http/interceptors into [http/json-body])))
+                      ::http/port   port}
+                     ))
 
 (defn -main [_] (-> service-map http/create-server http/start))
